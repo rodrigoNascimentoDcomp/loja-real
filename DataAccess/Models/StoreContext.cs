57514db36218b1c0d37f9de093c7c28c9870645a
rodrigoNascimentoDcomp <rodrigo.nascimento@dcomp.ufs.br>
2021-10-03 15:49:53 -0300

    Configure Fluent API

--- DataAccess/Models/StoreContext.cs
+++ DataAccess/Models/StoreContext.cs
@@ -18,5 +18,64 @@ namespace DataAccess.Models
         public DbSet<OrderItem> OrderItems { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductCategory> ProductCategories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Item>()
+                .HasKey(x => x.Id);
+
+            modelBuilder.Entity<Item>()
+                .Property(x => x.Price)
+                .HasPrecision(6, 2);
+
+            modelBuilder.Entity<Item>()
+                .HasOne(x => x.Product)
+                .WithMany()
+                .HasForeignKey(x => x.Product);
+
+            modelBuilder.Entity<Order>()
+                .HasKey(x => x.Id);
+
+            modelBuilder.Entity<Order>()
+                .HasMany(x => x.Items)
+                .WithOne(x => x.Order);
+
+            modelBuilder.Entity<Order>()
+                .Property(x => x.PriceTotal)
+                .HasPrecision(6, 2);
+
+            modelBuilder.Entity<Order>()
+                .Property(x => x.DiscountTotal)
+                .HasPrecision(6, 2);
+
+            modelBuilder.Entity<Order>()
+                .Ignore(x => x.OrderTotal);
+
+            modelBuilder.Entity<Order>()
+                .Property(x => x.Status)
+                .HasConversion<int>();
+
+            modelBuilder.Entity<OrderItem>()
+                .HasKey(x => x.Id);
+            
+            modelBuilder.Entity<OrderItem>()
+                .HasOne(x => x.Item)
+                .WithMany()
+                .HasForeignKey(x => x.Item);
+
+            modelBuilder.Entity<OrderItem>()
+                .HasOne(x => x.Order)
+                .WithMany()
+                .HasForeignKey(x => x.Order);
+
+            modelBuilder.Entity<OrderItem>()
+                .Property(x => x.Discount)
+                .HasPrecision(6, 2);
+
+            modelBuilder.Entity<OrderItem>()
+                .Ignore(x => x.Total);
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
