61f397145ebe5aafedde047b1dd64d742c2ea775
rodrigoNascimentoDcomp <rodrigo.nascimento@dcomp.ufs.br>
2021-10-07 18:19:35 -0300

    Add UnitOfWork

---
+++ DataAccess/UnitOfWork/Concrete/UnitOfWork.cs
@@ -0,0 +1,39 @@
+﻿using DataAccess.Models;
+using DataAccess.UnitOfWork.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataAccess.UnitOfWork.Concrete
+{
+    public class UnitOfWork : IUnitOfWork, IDisposable
+    {
+        private StoreContext context = new StoreContext();
+
+        public int Save()
+        {
+            return context.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
