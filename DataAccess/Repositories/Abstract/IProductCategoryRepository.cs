f28b26aeb1a37e8e69b89835aec7cfe6e24d0566
rodrigoNascimentoDcomp <rodrigo.nascimento@dcomp.ufs.br>
2021-10-06 21:36:14 -0300

    Add ProductCategoryRepository

---
+++ DataAccess/Repositories/Abstract/IProductCategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories.Abstract
+{
+    public interface IProductCategoryRepository
+    {
+    }
+}
---
+++ DataAccess/Repositories/Concrete/ProductCategoryRepository.cs
@@ -0,0 +1,44 @@
+﻿using DataAccess.Models;
+using DataAccess.Repositories.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories.Concrete
+{
+    public class ProductCategoryRepository : GenericRepository<ProductCategory>, IProductCategoryRepository, IDisposable
+    {
+        private readonly StoreContext _context;
+
+        public ProductCategoryRepository(StoreContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
