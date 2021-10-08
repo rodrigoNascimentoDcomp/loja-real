565d6c3ce7ec7c3257e789feea919c80ce821d23
rodrigoNascimentoDcomp <rodrigo.nascimento@dcomp.ufs.br>
2021-10-07 21:00:28 -0300

    Add GenericService

---
+++ Service/Abstract/IGenericService.cs
@@ -0,0 +1,29 @@
+﻿using DataAccess.Models;
+using DataAccess.Repositories.Abstract;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Linq.Expressions;
+
+namespace Service.Abstract
+{
+    public interface IGenericService<T> where T : class
+    {
+        IEnumerable<T> Get(
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            string includeProperties = "");
+
+        T GetByID(object id);
+
+        void Insert(T entity);
+
+        void Delete(object id);
+
+        void Delete(T entityToDelete);
+
+        void Update(T entityToUpdate);
+    }
+}
---
+++ Service/Concrete/GenericService.cs
@@ -0,0 +1,37 @@
+﻿using DataAccess.Repositories.Abstract;
+using DataAccess.UnitOfWork.Abstract;
+using Service.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Service.Concrete
+{
+    public class GenericService<T> : IGenericService<T> where T : class
+    {
+        private readonly IGenericRepository<T> _repository;
+
+        public GenericService(IGenericRepository<T> repository, IUnitOfWork unitOfWork)
+        {
+            _repository = repository;
+        }
+
+        public IEnumerable<T> Get(
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            string includeProperties = "") =>
+            _repository.Get(filter, orderBy, includeProperties);
+
+        public T GetByID(object id) => _repository.GetByID(id);
+
+        public void Insert(T entity) => _repository.Insert(entity);
+
+        public void Delete(object id) => _repository.Delete(id);
+
+        public void Delete(T entityToDelete) => _repository.Delete(entityToDelete);
+
+        public void Update(T entityToUpdate) => _repository.Update(entityToUpdate);
+    }
+}
