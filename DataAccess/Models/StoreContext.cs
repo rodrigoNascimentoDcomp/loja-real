ceabe147d3de7e446e585641e08d66127b4a334a
rodrigoNascimentoDcomp <rodrigo.nascimento@dcomp.ufs.br>
2021-10-09 15:17:44 -0300

    Add in memory database

--- DataAccess/Models/StoreContext.cs
+++ DataAccess/Models/StoreContext.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.InMemory;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -85,6 +88,20 @@ namespace DataAccess.Models
                 .HasForeignKey(x => x.ProductCategoryId);
 
             base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                IConfigurationRoot configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+
+                var connectionString = configuration.GetConnectionString("ConnectionString");
+                optionsBuilder.UseInMemoryDatabase(connectionString);
+            }
         }
     }
 }
