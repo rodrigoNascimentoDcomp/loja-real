029cd053d12fda8afb629c3e25af309694fc6c7a
rodrigoNascimentoDcomp <rodrigo.nascimento@dcomp.ufs.br>
2021-10-09 22:04:56 -0300

    Add ProductService

---
+++ Service/Abstract/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using DataAccess.Models;
+using DataAccess.Repositories.Abstract;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Linq.Expressions;
+
+namespace Service.Abstract
+{
+    public interface IProductService : IGenericService<Product>
+    {
+    }
+}
---
+++ Service/Concrete/ProductService.cs
@@ -0,0 +1,35 @@
+﻿using DataAccess.Models;
+using DataAccess.Repositories;
+using DataAccess.UnitOfWork.Abstract;
+using Service.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Service.Concrete
+{
+    public class ProductService : IProductService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductService(IUnitOfWork unitOfWork) => _unitOfWork = unitOfWork;
+
+        public IEnumerable<Product> Get(
+            Expression<Func<Product, bool>> filter = null,
+            Func<IQueryable<Product>, IOrderedQueryable<Product>> orderBy = null,
+            string includeProperties = "") =>
+            _unitOfWork.ProductRepository.Get(filter, orderBy, includeProperties);
+
+        public Product GetByID(int id) => _unitOfWork.ProductRepository.GetByID(id);
+
+        public void Insert(Product entity) => _unitOfWork.ProductRepository.Insert(entity);
+
+        public void Delete(int id) => _unitOfWork.ProductRepository.Delete(id);
+
+        public void Update(Product entityToUpdate) => _unitOfWork.ProductRepository.Update(entityToUpdate);
+
+        public void Save() => _unitOfWork.ProductRepository.Save();
+    }
+}
