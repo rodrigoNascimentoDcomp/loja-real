038006a4e1eda2f18da86a119d92a04d4089f143
rodrigoNascimentoDcomp <rodrigo.nascimento@dcomp.ufs.br>
2021-12-17 20:58:03 -0300

    Create Categorium pages

--- LojaReal/Controllers/CategoriumController.cs
+++ LojaReal/Controllers/CategoriumController.cs
@@ -3,6 +3,11 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Service.Abstract;
 using System.Diagnostics;
+using AutoMapper;
+using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace LojaReal.Controllers
 {
@@ -27,6 +32,69 @@ namespace LojaReal.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet]
+        public IActionResult Cadastro()
+        {
+            return View("Cadastro");
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Categorium categoria)
+        {
+            if (!ModelState.IsValid)
+                throw new InvalidOperationException();
+
+            if (categoria == null)
+                throw new ArgumentNullException(nameof(categoria));
+
+            _categoriumService.Insert(categoria);
+            _categoriumService.Save();
+
+            return RedirectToAction("Index", "Categorium");
+        }
+
+        public IActionResult Alteracao(int id)
+        {
+            var categoria = _categoriumService.GetByID(id);
+
+            if (categoria == null)
+                throw new InvalidOperationException();
+
+            return View("Alteracao", categoria);
+        }
+
+        [HttpPost]
+        public IActionResult Alterar(Categorium categoria)
+        {
+            if (!ModelState.IsValid)
+                throw new InvalidOperationException();
+
+            if (categoria == null)
+                throw new ArgumentNullException(nameof(categoria));
+
+            _categoriumService.Update(categoria);
+            _categoriumService.Save();
+
+            return RedirectToAction("Index", "Categorium");
+        }
+
+        [HttpPost]
+        public IActionResult Remover(int id)
+        {
+            if (!ModelState.IsValid)
+                throw new InvalidOperationException();
+
+            var produto = _categoriumService.GetByID(id);
+
+            if (produto == null)
+                throw new InvalidOperationException();
+
+            _categoriumService.Delete(produto.Id);
+            _categoriumService.Save();
+
+            return RedirectToAction("Index", "Categorium");
         }
     }
 }
